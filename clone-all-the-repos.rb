f85e4cc101d41252f06d0bbac5cadb7e8de28f9c
Andrew Kreps <andrew.kreps@gmail.com>
2014-10-01 13:55:22 -0700

    Added status.

--- clone-all-the-repos.rb
+++ clone-all-the-repos.rb
@@ -2,6 +2,7 @@ require 'json'
 
 all_the_repos = JSON.parse( IO.read(ARGV[0]) )
 
-all_the_repos.each do |repo|
+all_the_repos.each_with_index do |repo, index|
+  puts "#{index.to_f / all_the_repos.count.to_f * 100}%"
   system "git clone git@github.com:#{repo['full_name']}"
 end
