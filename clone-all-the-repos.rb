e80f44301a942b88f6a04966356155526e072412
Andrew Kreps <andrew.kreps@gmail.com>
2015-07-01 12:02:59 -0700

    Updated to use the github_api gem.  Autopagination ftw!

--- clone-all-the-repos.rb
+++ clone-all-the-repos.rb
@@ -1,8 +1,17 @@
 require 'json'
+require 'github_api'
 
-all_the_repos = JSON.parse( IO.read(ARGV[0]) )
+# all_the_repos = JSON.parse( IO.read(ARGV[0]) )
 
-all_the_repos.each_with_index do |repo, index|
-  puts "#{index.to_f / all_the_repos.count.to_f * 100}%"
-  system "git clone git@github.com:#{repo['full_name']}"
+repos    = Github::Client::Repos.new auto_pagination: true
+response = repos.list user: 'mixpo'
+
+response.each_page do |page|
+  page.each do |repo|
+    if File.exists? repo.name
+      continue
+    else
+      exec "git clone git@github.com:mixpo/#{repo.name}"
+    end
+  end
 end
